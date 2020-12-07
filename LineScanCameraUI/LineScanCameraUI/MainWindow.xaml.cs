bb3e22c2f2d4df169294c012f54a5b7f5ada1928
鑫建 孙 <xinjian_sun@sina.com>
2020-12-07 11:03:46 +0800

    软件常规功能添加完成

--- LineScanCameraUI/LineScanCameraUI/MainWindow.xaml.cs
+++ LineScanCameraUI/LineScanCameraUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LineScanCameraUI.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HandyControl.Controls;
 
 namespace LineScanCameraUI
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : HandyControl.Controls.Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            if (HandyControl.Controls.MessageBox.Show("你确定关闭软件吗？", "确认", MessageBoxButton.YesNo, MessageBoxImage.Question,MessageBoxResult.No) == MessageBoxResult.Yes)
+            {
+                System.Windows.Application.Current.Shutdown();
+            }
         }
     }
 }
---
+++ LineScanCameraUI/LineScanCameraUI/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,221 @@
+﻿using HalconDotNet;
+using HandyControl.Controls;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewROI;
+
+namespace LineScanCameraUI.ViewModel
+{
+    class MainWindowViewModel : NotificationObject
+    {
+        #region 属性绑定
+        private string version;
+
+        public string Version
+        {
+            get { return version; }
+            set
+            {
+                version = value;
+                this.RaisePropertyChanged("Version");
+            }
+        }
+        private string messageStr;
+
+        public string MessageStr
+        {
+            get { return messageStr; }
+            set
+            {
+                messageStr = value;
+                this.RaisePropertyChanged("MessageStr");
+            }
+        }
+        private bool statusCamera;
+
+        public bool StatusCamera
+        {
+            get { return statusCamera; }
+            set
+            {
+                statusCamera = value;
+                this.RaisePropertyChanged("StatusCamera");
+            }
+        }
+        private bool statusPLC;
+
+        public bool StatusPLC
+        {
+            get { return statusPLC; }
+            set
+            {
+                statusPLC = value;
+                this.RaisePropertyChanged("StatusPLC");
+            }
+        }
+        private string homePageVisibility;
+
+        public string HomePageVisibility
+        {
+            get { return homePageVisibility; }
+            set
+            {
+                homePageVisibility = value;
+                this.RaisePropertyChanged("HomePageVisibility");
+            }
+        }
+        private string parameterPageVisibility;
+
+        public string ParameterPageVisibility
+        {
+            get { return parameterPageVisibility; }
+            set
+            {
+                parameterPageVisibility = value;
+                this.RaisePropertyChanged("ParameterPageVisibility");
+            }
+        }
+        private HImage cameraIamge;
+
+        public HImage CameraIamge
+        {
+            get { return cameraIamge; }
+            set
+            {
+                cameraIamge = value;
+                this.RaisePropertyChanged("CameraIamge");
+            }
+        }
+        private bool cameraRepaint;
+
+        public bool CameraRepaint
+        {
+            get { return cameraRepaint; }
+            set
+            {
+                cameraRepaint = value;
+                this.RaisePropertyChanged("CameraRepaint");
+            }
+        }
+        private ObservableCollection<ROI> cameraROIList;
+
+        public ObservableCollection<ROI> CameraROIList
+        {
+            get { return cameraROIList; }
+            set
+            {
+                cameraROIList = value;
+                this.RaisePropertyChanged("CameraROIList");
+            }
+        }
+        private HObject cameraAppendHObject;
+
+        public HObject CameraAppendHObject
+        {
+            get { return cameraAppendHObject; }
+            set
+            {
+                cameraAppendHObject = value;
+                this.RaisePropertyChanged("CameraAppendHObject");
+            }
+        }
+        private Tuple<string, object> cameraGCStyle;
+
+        public Tuple<string, object> CameraGCStyle
+        {
+            get { return cameraGCStyle; }
+            set
+            {
+                cameraGCStyle = value;
+                this.RaisePropertyChanged("CameraGCStyle");
+            }
+        }
+        #endregion
+        #region 方法绑定
+        public DelegateCommand AppLoadedEventCommand { get; set; }
+        public DelegateCommand<object> MenuActionCommand { get; set; }
+        public DelegateCommand<object> OperateButtonCommand { get; set; }
+        #endregion
+        #region 构造函数
+        public MainWindowViewModel()
+        {
+            #region 初始化参数
+            Version = "20201207";
+            MessageStr = "";
+            HomePageVisibility = "Visible";
+            ParameterPageVisibility = "Collapsed";
+            CameraROIList = new ObservableCollection<ROI>();
+            #endregion
+            #region 初始化方法
+            AppLoadedEventCommand = new DelegateCommand(new Action(this.AppLoadedEventCommandExecute));
+            MenuActionCommand = new DelegateCommand<object>(new Action<object>(this.MenuActionCommandExecute));
+            OperateButtonCommand = new DelegateCommand<object>(new Action<object>(this.OperateButtonCommandExecute));
+            #endregion
+            #region 启动条件
+            System.Diagnostics.Process[] myProcesses = System.Diagnostics.Process.GetProcessesByName("LineScanCameraUI");//获取指定的进程名   
+            if (myProcesses.Length > 1) //如果可以获取到知道的进程名则说明已经启动
+            {
+                MessageBox.Show("不允许重复打开软件", "Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+                System.Windows.Application.Current.Shutdown();
+            }
+            #endregion
+        }
+
+        private void OperateButtonCommandExecute(object obj)
+        {
+            switch (obj.ToString())
+            {
+                case "0":
+                    AddMessage("待添加功能");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MenuActionCommandExecute(object obj)
+        {
+            switch (obj.ToString())
+            {
+                case "0":
+                    HomePageVisibility = "Visible";
+                    ParameterPageVisibility = "Collapsed";
+                    break;
+                case "1":
+                    HomePageVisibility = "Collapsed";
+                    ParameterPageVisibility = "Visible";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void AppLoadedEventCommandExecute()
+        {
+            //throw new NotImplementedException();
+            AddMessage("软件加载完成");
+        }
+        #endregion
+        #region 自定义函数
+        private void AddMessage(string str)
+        {
+            string[] s = MessageStr.Split('\n');
+            if (s.Length > 1000)
+            {
+                MessageStr = "";
+            }
+            if (MessageStr != "")
+            {
+                MessageStr += "\n";
+            }
+            MessageStr += System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + " " + str;
+        }
+        #endregion
+    }
+}
